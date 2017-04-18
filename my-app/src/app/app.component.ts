1dc3479eee45c47ff05d7be9da7100216ac17637
John <johnconley507@hotmail.com>
2017-04-18 16:58:43 -0400

    configurpageTitle

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'pm-app',
+	template: 
+	<div><h1>({pageTitle})</h1>
+		<div>My First Component</div>
+	</div>
+	
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent { 
+	pageTitle: string = 'Acme Product Mangement';
 }
