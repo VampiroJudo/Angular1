16e6ba4f26ec0cba4cee6daa8f17353aef6bbacf
John <johnconley507@hotmail.com>
2017-04-18 21:36:11 -0400

    bug fixes for render page

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'pm-app',
-	template: '
-	<div><h1>({pageTitle})</h1>
-		<div>My First Component</div>
-	</div>
-	'
-	
+    selector: 'pm-app',
+    template: `
+    <div>
+        <h1>Acme Product Management</h1>
+        <div> My First Component</div>
+    </div>
+    `
 })
-
-export class AppComponent { 
-	pageTitle: string = 'Acme Product Mangement';
-}
+export class AppComponent { }
