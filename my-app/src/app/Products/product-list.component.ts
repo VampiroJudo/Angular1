cbe818f5e6f84cc4dd8276196b060158c0122fa3
John <johnconley507@hotmail.com>
2017-04-25 23:37:08 -0400

    subscribing to the observable

--- my-app/src/app/Products/product-list.component.ts
+++ my-app/src/app/Products/product-list.component.ts
@@ -17,9 +17,11 @@ export class ProductListComponent implements OnInit{
 	imageMargin: number = 2;
 	showImage: boolean = false;
 	listFilter: string;
+	errorMessage: string;
+
 	products: IProduct[];
 
-constructor(private_productService: ProductService) {
+constructor(private _productService: ProductService) {
 	}
 
 toggleImage(): void {
@@ -27,7 +29,9 @@ toggleImage(): void {
 	}
 
 ngOnInit(): void {
-	this.products = this._productService.getProducts();
+	this._productService.getProducts()
+		.subscribe(products => this.products = products);
+					error => this.errorMessage = <any>error;
 	}
 
  onRatingClicked(message: string): void {
