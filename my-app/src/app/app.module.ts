6cb0decb9de7ad1cb9e20e34556396d9f7ec0701
John <johnconley507@hotmail.com>
2017-04-27 18:59:35 -0400

    setting up routes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -13,7 +14,13 @@ import { StarComponent } from './shared/star.component';
   	BrowserModule,
   	FormsModule,
     HttpModule
-  	 ],
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
+  ],
   declarations: [ 
   AppComponent,
   ProductListComponent,
